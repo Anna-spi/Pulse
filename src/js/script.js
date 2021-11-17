4a7da1202b30e6f65ae3ef484a873a2c42088e6d
Anna <aspirido4@gmail.com>
2021-11-17 14:18:50 +0200

    Added adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,41 @@
 // Слайдер с помощью Slick
-// $(document).ready(function () {
-//   $(".carousel__inner").slick({
-//     speed: 1200,
-//     adaptiveHeight: true,
-//     prevArrow:
-//       '<button type="button" class="slick-prev"><img src="icons/left_1.svg"></button>',
-//     nextArrow:
-//       '<button type="button" class="slick-next"><img src="icons/right_1.svg"></button>',
-//     responsive: [
-//       {
-//         breakpoint: 992,
-//         settings: {
-//           dots: true,
-//           arrows: false,
-//         },
-//       },
-//     ],
-//   });
-// });
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/left_1.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/right_1.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
 
 // Слайдер с помощью tiny-slider
-const slider = tns({
-  container: ".carousel__inner",
-  items: 1,
-  slideBy: "page",
-  autoplay: false,
-  controls: false,
-  nav: false,
-});
+// const slider = tns({
+//   container: ".carousel__inner",
+//   items: 1,
+//   slideBy: "page",
+//   autoplay: false,
+//   controls: false,
+//   nav: false,
+// });
 
-document.querySelector(".prev").addEventListener("click", function () {
-  slider.goTo("prev");
-});
+// document.querySelector(".prev").addEventListener("click", function () {
+//   slider.goTo("prev");
+// });
 
-document.querySelector(".next").addEventListener("click", function () {
-  slider.goTo("next");
-});
+// document.querySelector(".next").addEventListener("click", function () {
+//   slider.goTo("next");
+// });
 
 $(document).ready(function () {
   $("ul.catalog__tabs").on(
